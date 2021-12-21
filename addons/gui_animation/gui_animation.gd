0ea720251d89522183be9e44835f91ae26288b30
SK Source <79965488+sksourcedata@users.noreply.github.com>
2021-12-21 15:42:33 +0100

    Update gui_animation.gd

--- addons/gui_animation/gui_animation.gd
+++ addons/gui_animation/gui_animation.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("SlidingContainer", "MarginContainer", preload("sliding_container.gd"), preload("sliding_container.png"))
+	add_custom_type("TransformContainer", "MarginContainer", preload("transform_container.gd"), preload("transform_container.png"))
 
 func _exit_tree():
-	remove_custom_type("SlidingContainer")
+	remove_custom_type("TransformContainer")
