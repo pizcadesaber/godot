b465e3438b9c1590e3456d82412aa7772e6a858b
SK Source <79965488+sksourcedata@users.noreply.github.com>
2021-12-21 15:36:28 +0100

    Create sliding_container.gd
    Custom node SlidingContainer, which extends MarginContainer.

---
+++ addons/gui_animation/sliding_container.gd
@@ -0,0 +1,37 @@
+tool
+extends MarginContainer
+
+export(Vector2) var offset = Vector2(0,0) setget set_offset
+export(Vector2) var scale = Vector2(1,1) setget set_scale
+
+func set_offset(vector: Vector2):
+	if offset == vector:
+		return
+	offset = vector
+	set_transform(offset, scale)
+	#add_constant_override("margin_left", vector.x)
+	#add_constant_override("margin_right", -vector.x)
+	#add_constant_override("margin_bottom", vector.y)
+	#add_constant_override("margin_top", -vector.y)
+
+func set_scale(vector: Vector2):
+	if scale == vector:
+		return
+	scale = vector
+	set_transform(offset, scale)
+	#print(scale)
+	#var size = rect_size / 2
+	#add_constant_override("margin_left", (1-vector.x) * size.x)
+	#add_constant_override("margin_right", (1-vector.x) * size.x)
+
+func set_transform(offset_vector: Vector2, scale_vector: Vector2):
+	var size = rect_size / 2
+	scale_vector = (Vector2(1,1) - scale_vector)
+	add_constant_override("margin_left",
+			scale_vector.x * size.x + offset_vector.x)
+	add_constant_override("margin_right",
+			scale_vector.x * size.x - offset_vector.x)
+	add_constant_override("margin_bottom", 
+			scale_vector.y * size.y + offset_vector.y)
+	add_constant_override("margin_top", 
+			scale_vector.y * size.y - offset_vector.y)
