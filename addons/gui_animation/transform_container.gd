661ac7e90afbccd1865d17927d599fc5f3ec62fa
SK Source <79965488+sksourcedata@users.noreply.github.com>
2021-12-29 23:12:31 +0100

    Update transform_container.gd

--- addons/gui_animation/transform_container.gd
+++ addons/gui_animation/transform_container.gd
@@ -3,27 +3,33 @@ extends MarginContainer
 
 export(Vector2) var offset = Vector2(0,0) setget set_offset
 export(Vector2) var scale = Vector2(1,1) setget set_scale
+export(Vector2) var pivot = Vector2(0.5, 0.5) setget set_pivot
 
 func set_offset(vector: Vector2):
 	if offset == vector:
 		return
 	offset = vector
-	set_transform(offset, scale)
+	set_transform()
+
+func set_pivot(vector: Vector2):
+	if pivot == vector:
+		return
+	pivot = vector
+	set_transform()
 
 func set_scale(vector: Vector2):
 	if scale == vector:
 		return
 	scale = vector
-	set_transform(offset, scale)
+	set_transform()
 
-func set_transform(offset_vector: Vector2, scale_vector: Vector2):
-	var size = rect_size / 2
-	scale_vector = (Vector2(1,1) - scale_vector)
+func set_transform():
+	var vector = (Vector2(1,1) - scale)
 	add_constant_override("margin_left",
-			scale_vector.x * size.x + offset_vector.x)
+		vector.x * rect_size.x * pivot.x + offset.x)
 	add_constant_override("margin_right",
-			scale_vector.x * size.x - offset_vector.x)
+			 vector.x * rect_size.x * (1 - pivot.x) - offset.x)
 	add_constant_override("margin_bottom", 
-			scale_vector.y * size.y + offset_vector.y)
-	add_constant_override("margin_top", 
-			scale_vector.y * size.y - offset_vector.y)
+			vector.y * rect_size.y * pivot.y + offset.y)
+	add_constant_override("margin_top",
+			 vector.y * rect_size.y * (1 - pivot.y) - offset.y)
